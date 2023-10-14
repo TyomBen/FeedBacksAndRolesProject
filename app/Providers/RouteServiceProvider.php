162bf77b5b79d6aa37bbc49e8c5cc84cf51d5da8
Artyom <132067879+TyomBen@users.noreply.github.com>
2023-10-14 20:35:54 +0400

    little change

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -28,7 +28,7 @@ class RouteServiceProvider extends ServiceProvider
             return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
         });
         RateLimiter::for('feedback', function (Request $request) {
-            return Limit::perDay(1)->by($request->ip());
+            return Limit::perDay(1)->by($request->user()->id);
         });
 
         $this->routes(function () {
